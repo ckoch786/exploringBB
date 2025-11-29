c3005928ae36aa9f1af5dea4fb93147e88210a11
Cory Koch <cory.koch.yunus@gmail.com>
2025-11-29 04:15:23 +0000

    fix print function calls, add parens. Version differences?

--- chp05/pythonLED/pythonLED.py
+++ chp05/pythonLED/pythonLED.py
@@ -20,30 +20,30 @@ def removeTrigger():
    writeLED (filename="/trigger", value="none")
    return
 
-print "Starting the LED Python Script"
+print("Starting the LED Python Script")
 if len(sys.argv)!=2:
-   print "There are an incorrect number of arguments"
-   print "  usage is:  pythonLED.py command"
-   print "  where command is one of on, off, flash or status."
+   print("There are an incorrect number of arguments")
+   print("  usage is:  pythonLED.py command")
+   print("  where command is one of on, off, flash or status.")
    sys.exit(2)
 if sys.argv[1]=="on":
-   print "Turning the LED on"
+   print("Turning the LED on")
    removeTrigger()
    writeLED (filename="/brightness", value="1")
 elif sys.argv[1]=="off":
-   print "Turning the LED off"
+   print("Turning the LED off")
    removeTrigger()
    writeLED (filename="/brightness", value="0")
 elif sys.argv[1]=="flash":
-   print "Flashing the LED"
+   print("Flashing the LED")
    writeLED (filename="/trigger", value="timer")
    writeLED (filename="/delay_on", value="50")
    writeLED (filename="/delay_off", value="50")
 elif sys.argv[1]=="status":
-   print "Getting the LED trigger status"
+   print("Getting the LED trigger status")
    fo = open( LED3_PATH + "/trigger", "r")
-   print fo.read()
+   print(fo.read())
    fo.close()
 else:
-   print "Invalid Command!"
-print "End of Python Script"
+   print("Invalid Command!")
+print("End of Python Script")
